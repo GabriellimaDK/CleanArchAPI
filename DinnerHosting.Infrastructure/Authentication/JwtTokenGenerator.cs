f0ad7588f1d7a2d1cc07f5753c558c896ac27db2
Gabriel <gabriellimadk@yahoo.com.br>
2024-01-14 15:51:27 +0000

    create DateTime provider app interface and infra implementation using DateTime provider in jwt generator

--- DinnerHosting.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ DinnerHosting.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using DinnerHosting.Application.Common.Interfaces.Authentication;
+using DinnerHosting.Application.Common.Interfaces.Services;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -8,6 +9,13 @@ namespace DinnerHosting.Infrastructure.Authentication
 {
     public class JwtTokenGenerator : IJwtTokenGenerator
     {
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public JwtTokenGenerator(IDateTimeProvider dateTimeProvider)
+        {
+            _dateTimeProvider = dateTimeProvider;
+        }
+
         public string GenerateToken(Guid userId, string firstName, string lastName)
         {
             var signingCredentials = new SigningCredentials(
@@ -25,7 +33,7 @@ namespace DinnerHosting.Infrastructure.Authentication
 
             var securityToken = new JwtSecurityToken(
                 issuer: "DinnerHosting",
-                expires: DateTime.Now.AddDays(1),
+                expires: _dateTimeProvider.UtcNow.AddMinutes(60),
                 claims: claims,
                 signingCredentials: signingCredentials
                 );
--- DinnerHosting.Infrastructure/DependencyInjection.cs
+++ DinnerHosting.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using DinnerHosting.Application.Common.Interfaces.Authentication;
+using DinnerHosting.Application.Common.Interfaces.Services;
 using DinnerHosting.Infrastructure.Authentication;
+using DinnerHosting.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DinnerHosting.Infrastructure
@@ -9,6 +11,7 @@ namespace DinnerHosting.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             return services;
         }
     }
